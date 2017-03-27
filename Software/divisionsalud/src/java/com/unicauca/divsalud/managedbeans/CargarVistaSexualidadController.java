85c322f1a508dfc990bc4baef3fe4d96372c2142
DanielaVG <angiedanielav@unicauca.edu.co>
2017-03-26 23:07:24 -0500

    Menu

---
+++ Software/divisionsalud/src/java/com/unicauca/divsalud/managedbeans/CargarVistaSexualidadController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unicauca.divsalud.managedbeans;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Daniela
+ */
+@Named(value = "cargarVistaSexualidad")
+@SessionScoped
+public class CargarVistaSexualidadController implements Serializable
+{
+
+    private String ruta; 
+    public CargarVistaSexualidadController()
+    {
+    }
+    
+    public void cargarRegistrarPaciente()
+    {
+        ruta = "/usuariodelsistema/profesionalsalud/enComun/paciente/Create.xhtml";
+    }
+    public void cargarListarPaciente()
+    {
+        //ruta = "/usuariodelsistema/profesionalsalud/enComun/paciente/List.xhtml";
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+    
+}
