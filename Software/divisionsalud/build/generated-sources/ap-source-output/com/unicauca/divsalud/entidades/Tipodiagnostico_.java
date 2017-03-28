b23884e657f896d4f0aa53a2f93d7d09cee6a26d
DanielaVG <angiedanielav@unicauca.edu.co>
2017-03-28 00:48:54 -0500

    inf basica en el registro de paciente agrego departamento y municipio

--- Software/divisionsalud/build/generated-sources/ap-source-output/com/unicauca/divsalud/entidades/Tipodiagnostico_.java
+++ Software/divisionsalud/build/generated-sources/ap-source-output/com/unicauca/divsalud/entidades/Tipodiagnostico_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-27T23:09:39")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-28T00:47:43")
 @StaticMetamodel(Tipodiagnostico.class)
 public class Tipodiagnostico_ { 
 
