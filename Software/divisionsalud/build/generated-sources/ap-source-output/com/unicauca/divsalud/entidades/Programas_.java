a36428da6791cd4a22f205f93caf45d1a3322e75
DavidMGomez <david-gomez12@hotmail.com>
2017-03-29 14:26:21 -0500

    Correcciones al formulario de registrar Paciente

--- Software/divisionsalud/build/generated-sources/ap-source-output/com/unicauca/divsalud/entidades/Programas_.java
+++ Software/divisionsalud/build/generated-sources/ap-source-output/com/unicauca/divsalud/entidades/Programas_.java
@@ -7,11 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-28T21:34:31")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-28T12:25:22")
->>>>>>> b4a99cc2c42d2fe96d5980b09237ebe3859e2c66
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-29T14:04:54")
 @StaticMetamodel(Programas.class)
 public class Programas_ { 
 
