435f5e57b785ba5ebbb0c01a0e1f9e3133c8a2e3
DanielaVG <angiedanielav@unicauca.edu.co>
2017-03-28 21:37:03 -0500

    registro funciona

--- Software/divisionsalud/build/generated-sources/ap-source-output/com/unicauca/divsalud/entidades/Diagnosticocie10Odo_.java
+++ Software/divisionsalud/build/generated-sources/ap-source-output/com/unicauca/divsalud/entidades/Diagnosticocie10Odo_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-28T02:38:20")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-28T21:34:31")
 @StaticMetamodel(Diagnosticocie10Odo.class)
 public class Diagnosticocie10Odo_ { 
 
